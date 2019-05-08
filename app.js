465aed34abacbef832fbfe90a67237ba0fc8306e
computershawn <shontek@gmail.com>
2019-05-07 21:53:46 -0700

    Move application data to external file

--- app.js
+++ app.js
@@ -1,60 +1,26 @@
 'use strict'
 
-// ALL MY DATAS
-const experienceData = [
-  {
-    title: "Interaction Designer /__ CivicConnect /__ March 2017 – Present",
-    details: "Responsive design of sites and web apps related to regional transportation and commerce. User interface designer for FreightWorks and Active Transportation Database (ATDB) sites for Southern California Association of Governments. Developed workflow and visual design of mobile app for counting pedestrians and cyclists. Ongoing support of Metropolitan Transportation Commission’s 511.org web app, including UI/UX design, image and HTML/CSS/JS editing, contributing with GitHub, and user acceptance testing.",
-  },
-  {
-    title: "Data Visualization Designer /__ Pitch Interactive /__ Aug 2016 – Feb 2017",
-    details: "Involved in all aspects of projects, from initial exploration to delivery of final graphics and high-fidelity wireframes. Rapid visual ideation using pen and paper, creative software, and code. Regularly interface with creative director and with clients, presenting multiple options and synthesizing feedback. Created conceptual designs and final infographic for Black Spectacles’ 2016 software requirements study. Responsive layout and image production for Facebook Insights More Than a Message website. Refined interface for Pitch Interactive’s Tilegrams web-based hexagonal tile mapping tool.",
-  },
-];
 
-const educationData = [
-  {
-    title: "Art Center College of Design /__ Pasadena, CA /__ 2014",
-    details: "Master of Fine Arts, Media Design Practices",
-  },
-  {
-    title: "University of California /__ Los Angeles, CA /__ 2001",
-    details: "2014 Bachelor of Science, Mechanical Engineering",
-  },
-];
 
-const skillsData = [
-  {
-    category: "Design + Presentation",
-    details: "Wireframing, Prototyping, Information Architecture, Layout, Iconography",
-  },
-  {
-    category: "Creative Software",
-    details: "Sketch, InVision, Adobe Creative Suite, After Effects, Framer",
-  },
-  {
-    category: "Programming",
-    details: "HTML, CSS, JavaScript, D3.js, ActionScript, Processing, Python, Arduino",
-  }
-];
 
 // Define the main App Component
 function App(props) {
+  //console.log(props.theInfo);
   return (
     <main role="main">
       <Avi /><Hello />
       <hr/>
-      <Experience theInfo={experienceData} />
+      <Experience theInfo={props.theInfo.experienceData} />
       <hr/>
-      <Education theInfo={educationData} />
+      <Education theInfo={props.theInfo.educationData} />
       <hr/>
-      <Skills theInfo={skillsData} />
+      <Skills theInfo={props.theInfo.skillsData} />
   	</main>
   );
 }
 
 // Define an Avatar Component
-function Avi(props) {
+function Avi() {
   return (
     <section>
       <img id="profile-pic" src="images/shawnj_selfie_sm.jpg" width="160" alt="shawn profile pic"/>
@@ -126,6 +92,6 @@ function Skills(props) {
 
 // Nested elements inside the App
 ReactDOM.render(
-  <App />,
+  <App theInfo={allData}/>,
   document.querySelector('#application-root')
 );
---
+++ dataStore.js
@@ -0,0 +1,39 @@
+'use strict'
+
+// ALL MY DATAS
+const allData = {
+    experienceData: [
+      {
+        title: "Interaction Designer /__ CivicConnect /__ March 2017 – Present",
+        details: "Responsive design of sites and web apps related to regional transportation and commerce. User interface designer for FreightWorks and Active Transportation Database (ATDB) sites for Southern California Association of Governments. Developed workflow and visual design of mobile app for counting pedestrians and cyclists. Ongoing support of Metropolitan Transportation Commission’s 511.org web app, including UI/UX design, image and HTML/CSS/JS editing, contributing with GitHub, and user acceptance testing.",
+      },
+      {
+        title: "Data Visualization Designer /__ Pitch Interactive /__ Aug 2016 – Feb 2017",
+        details: "Involved in all aspects of projects, from initial exploration to delivery of final graphics and high-fidelity wireframes. Rapid visual ideation using pen and paper, creative software, and code. Regularly interface with creative director and with clients, presenting multiple options and synthesizing feedback. Created conceptual designs and final infographic for Black Spectacles’ 2016 software requirements study. Responsive layout and image production for Facebook Insights More Than a Message website. Refined interface for Pitch Interactive’s Tilegrams web-based hexagonal tile mapping tool.",
+      }
+    ],
+    educationData: [
+      {
+        title: "Art Center College of Design /__ Pasadena, CA /__ 2014",
+        details: "Master of Fine Arts, Media Design Practices",
+      },
+      {
+        title: "University of California /__ Los Angeles, CA /__ 2001",
+        details: "2014 Bachelor of Science, Mechanical Engineering",
+      },
+    ],
+    skillsData: [
+      {
+        category: "Design + Presentation",
+        details: "Wireframing, Prototyping, Information Architecture, Layout, Iconography",
+      },
+      {
+        category: "Creative Software",
+        details: "Sketch, InVision, Adobe Creative Suite, After Effects, Framer",
+      },
+      {
+        category: "Programming",
+        details: "HTML, CSS, JavaScript, D3.js, ActionScript, Processing, Python, Arduino",
+      }
+    ]
+  };
